cac1679d6aaf2b621f91867dc1e116be049fab51
Joseph Yoon <sidnancy81@gmail.com>
2024-11-02 12:09:44 +0900

    initial commit

---
+++ src/main/kotlin/space/greyempty/magnetic/MagneticApplication.kt
@@ -0,0 +1,11 @@
+package space.greyempty.magnetic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MagneticApplication
+
+fun main(args: Array<String>) {
+	runApplication<MagneticApplication>(*args)
+}
---
+++ src/test/kotlin/space/greyempty/magnetic/MagneticApplicationTests.kt
@@ -0,0 +1,13 @@
+package space.greyempty.magnetic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MagneticApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
